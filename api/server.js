07f53d5ad6b654a167909c8882f73efbfa2fd421
kal <sadkalshayee@gmail.com>
2024-12-14 16:34:27 -0800

    comment out app.listen

--- api/server.js
+++ api/server.js
@@ -83,6 +83,6 @@ export default (req, res) => {
   app(req, res);
 };
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
