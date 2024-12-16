e46da1e4d705b47e1630da07c45b529f69e7a6c9
kal <sadkalshayee@gmail.com>
2024-12-15 16:18:19 -0800

    modify vercel again and add routes[]

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ import dotenv from "dotenv";
 import logger from "./logger.js";
 import route from "./route.js";
 dotenv.config();
+const port = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://kal-aa.github.io"],
+  })
+);
 app.use(bodyParser.json());
 app.use(logger);
 app.use(route);
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log("You're listening to port", PORT);
+app.listen(port, () => {
+  console.log("You're listening to port", port);
 });
